fc16d455ca284b79672d6d2551f5ca40b17b99c5
Thomas Kissane <thomas.kissane@marquette.edu>
2020-01-14 14:03:44 -0600

    committing previous code written

---
+++ HelloWorld/Hello.c
@@ -0,0 +1,18 @@
+#include <stdio.h> // we need this for printf function
+
+/*This is my first multi-line comment
+in my first C program*/
+
+// this is the main entry point for my application
+void main()
+
+{// this is the beginning of the main function
+	printf("Hello, World!\n");
+
+    // char type
+    printf("char size = %d byte \n", sizeof(char));
+    printf("signed char size = %d byte \n", sizeof(signed char));
+    printf("unsigned char size = %d byte \n", sizeof(unsigned char));
+}// this is the end of the main function
+
+// end of "Hello.c"
