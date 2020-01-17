7fc8a1712ce1140c2471b09c04f0f1de7181a2af
tkissane <tkissane@marquette.edu>
2020-01-17 13:33:49 -0600

    Added different types of data types and their sizes

--- HelloWorld/Hello.c
+++ HelloWorld/Hello.c
@@ -13,6 +13,29 @@ void main()
     printf("char size = %d byte \n", sizeof(char));
     printf("signed char size = %d byte \n", sizeof(signed char));
     printf("unsigned char size = %d byte \n", sizeof(unsigned char));
+
+    // int type
+    printf("int size = %d byte \n", sizeof(int));
+    printf("signed int size = %d byte \n", sizeof(signed int));
+    printf("unsigned int size = %d byte \n", sizeof(unsigned int));
+
+    // short in
+    printf("short int size = %d byte\n", sizeof(short int));
+    printf("signed short int size = %d byte\n", sizeof(signed short int));
+    printf("unsigned short int size = %d byte\n", sizeof(unsigned short int));
+    printf("unsigned short int size = %d byte\n", sizeof(int short unsigned));
+
+    // long int
+    printf("long int size = %d byte\n", sizeof(long int));
+
+    // long long
+    printf("long long size = %d byte\n", sizeof(long long));
+
+    // float type
+    printf("float size = %d byte\n", sizeof(float));
+    printf("double size = %d byte\n", sizeof(double));
+
 }// this is the end of the main function
+
 
 // end of "Hello.c"
